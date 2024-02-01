f2df87759cffb145be2e10b82913b3c459539b36
ingridLima <ingriddlima2000@gmail.com>
2024-02-01 14:25:13 -0300

    first commit

---
+++ atividade.java
@@ -0,0 +1,36 @@
+public class Atividades {
+    public static void main(String[] args) {
+        System.out.println(">>> listagem dos clientes");
+
+        System.out.println("________");
+        String nome1 = "fulano";
+        int data1 = 1980;
+
+        System.out.println("nome: " + nome1);
+
+        System.out.println("data de nascimento: " + data1);
+
+        System.out.println("________");
+        String nome2 = " sicrano";
+        int data2 = 2000;
+
+        System.out.println("nome: " + nome2);
+        System.out.println("nome: " + data2);
+   }
+   
+}
+
+public class Atividade {
+    public static void main(String[] args) {
+        System.out.println(">>> Listagem dos produtos");
+        System.out.println("_____");
+        String nomeLivro = "Livro";
+        double precoLivro = 49.95;
+        String nomeCelular = "Celular";
+        double precoCelular = 1470.76;
+
+        System.out.println("Nome: " + nomeLivro);
+        System.out.println("Preço: R$ " + precoLivro);
+        System.out.println("_____");
+    }
+}
